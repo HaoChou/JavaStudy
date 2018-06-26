8802a40e7f019de8f33062f688345b965c2a85af
zhouhao <zhouhao@corp.rrzcp8.com>
2018-06-26 14:28:29 +0800

    asm test

---
+++ asm/src/main/java/asm/test/AsmTester.java
@@ -0,0 +1,12 @@
+package asm.test;
+
+/**
+ * @author zhou
+ * Created on 2018/6/25
+ */
+public class AsmTester {
+    public void run()
+    {
+        System.out.println("This is my first ASM test class");
+    }
+}
