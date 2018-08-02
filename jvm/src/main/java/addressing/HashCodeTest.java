f9278334751794afb56c19576411bd16c0d1de3f
zhouhao <zhouzhou1992@live.com>
2018-08-02 14:28:05 +0800

    hashcode冲突

---
+++ jvm/src/main/java/addressing/HashCodeTest.java
@@ -0,0 +1,29 @@
+package addressing;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author zhou
+ * Created on 2018/8/2
+ */
+public class HashCodeTest {
+
+
+
+    public static void main(String[] args)
+    {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 300000; i++)
+        {
+            HashCodeTest p = new HashCodeTest();
+            if (!set.contains(p.hashCode()))
+                set.add(p.hashCode());
+        }
+        System.out.println(set.size());
+        //299981
+    }
+
+}
