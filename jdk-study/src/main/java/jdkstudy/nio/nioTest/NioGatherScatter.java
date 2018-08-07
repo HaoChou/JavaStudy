0603d9af9a33af295e66e9ef16379998d5dc9b0d
zhouhao <zhouzhou1992@live.com>
2018-08-07 17:30:52 +0800

    NioGatherScatter

---
+++ jdk-study/src/main/java/jdkstudy/nio/nioTest/NioGatherScatter.java
@@ -0,0 +1,45 @@
+package jdkstudy.nio.nioTest;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author zhou
+ * Created on 2018/8/7
+ */
+public class NioGatherScatter {
+
+    public static void main(String[] args)  {
+        RandomAccessFile aFile = null;
+        try {
+            //这个文件可以忽略
+            aFile = new RandomAccessFile("src/main/java/jdkstudy/nio/nioTest/nioTest.txt", "rw");
+
+            FileChannel fileChannel = aFile.getChannel();
+            ByteBuffer header = ByteBuffer.allocate(128);
+            ByteBuffer body = ByteBuffer.allocate(1024);
+            ByteBuffer[] byteBufferArray = {header, body};
+
+            fileChannel.read(byteBufferArray);//scattering reads
+
+            fileChannel.write(byteBufferArray);//gathering write
+        }
+        catch (Exception e)
+        {
+
+        }
+        finally {
+            try {
+                if (aFile != null) {
+                    aFile.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
