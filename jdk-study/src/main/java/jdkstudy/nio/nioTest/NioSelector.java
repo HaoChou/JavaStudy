1f4a6bd4a122f778dad94da3a461459362fd41b2
zhouhao <zhouzhou1992@live.com>
2018-08-16 21:39:51 +0800

    Nio-Selector

---
+++ jdk-study/src/main/java/jdkstudy/nio/nioTest/NioSelector.java
@@ -0,0 +1,55 @@
+package jdkstudy.nio.nioTest;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+/**
+ * @author zhou
+ * Created on 2018/8/16
+ */
+public class NioSelector {
+
+
+    public static void main(String[] args) throws IOException {
+        try (Selector selector = Selector.open();
+             ServerSocketChannel channel = ServerSocketChannel.open();) {
+
+
+            //绑定本地端口
+            channel.socket().bind(new InetSocketAddress(2222),1024);
+
+            channel.configureBlocking(false);
+            SelectionKey register = channel.register(selector, SelectionKey.OP_ACCEPT);
+
+            boolean isRunning =true;
+
+            while (isRunning){
+                selector.select();
+
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+
+                while (iterator.hasNext()){
+
+                    final SelectionKey key = iterator.next();
+                    iterator.remove();
+                    if(key.isAcceptable()) {
+
+                        final SocketChannel socketChannel = channel.accept();
+                        //todo
+                        // a connection was accepted by a ServerSocketChannel.
+                    } else if (key.isConnectable()) {
+                        // a connection was established with a remote server.
+                    } else if (key.isReadable()) {
+                        // a channel is ready for reading
+                    } else if (key.isWritable()) {
+                        // a channel is ready for writing
+                    }
+
+                }
+            }
+        }
+    }
+}
