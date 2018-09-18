6b7dc361c86302d21eb922da0d013345aff459b2
zhouhao <zhouzhou1992@live.com>
2018-09-18 17:37:13 +0800

    groovy-pom

---
+++ jvm/src/main/java/groovy/codecachetest/GroovyCodeCacheTest.java
@@ -0,0 +1,13 @@
+package groovy.codecachetest;
+
+/**
+ * @author zhou
+ * Created on 2018/9/18
+ */
+public class GroovyCodeCacheTest {
+
+    public static void main(String[] args) {
+
+        GroovyClassLoader groovyClassLoader = new GroovyClassLoader();
+    }
+}
